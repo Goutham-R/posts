17e2c590d6b48bbf5b9fa92e36aec0999ff719af
goutham_r <gouthamr000@gmail.com>
2020-07-28 20:34:49 +0530

    Updates

---
+++ precache-manifest.602925bdd36c6cbba4e175cc942b8785.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1601dc652f5eaad89fef4cc4a2ee5508",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "a24e70c75b0cf5ad68c0",
+    "url": "/my-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bca313372de2717f6422",
+    "url": "/my-app/static/css/main.df6f254e.chunk.css"
+  },
+  {
+    "revision": "a24e70c75b0cf5ad68c0",
+    "url": "/my-app/static/js/2.e303ce7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.e303ce7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bca313372de2717f6422",
+    "url": "/my-app/static/js/main.13d79566.chunk.js"
+  },
+  {
+    "revision": "30776b7a3f1db488c0e3",
+    "url": "/my-app/static/js/runtime-main.e309502f.js"
+  }
+]);
